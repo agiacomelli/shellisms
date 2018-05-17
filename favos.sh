9ccf34c5bcea6b771068046f42945b90034d22b7
agiacomelli <36065626+agiacomelli@users.noreply.github.com>
2018-05-17 10:28:53 +0200

    Create favos.sh

---
+++ favos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# the list of my favourite linux apps and how i install them on a new arch system with just one cmd
+# cherrytree
+# owncloud-client
+# opera
+# geany
+# geary
+# .....
+
+sudo pacman -S cherrytree owncloud-client opera geany geary nodejs npm atom scribus inkscape mplayer whois curl neofetch pluma vim tor 
