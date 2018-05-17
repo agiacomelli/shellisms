3be56dd94b1ca37da447aac0596959d27dbf5d92
agiacomelli <36065626+agiacomelli@users.noreply.github.com>
2018-05-17 09:55:45 +0200

    Update and rename pdfmerge to pdfmerge.sh

--- pdfmerge.sh
+++ pdfmerge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #
 # A SIMPLE SCRIPT TO merge 2 pdf's together.
-# USAGE: ./pdfmerge file1.pdf file2.pdf output_file.pdf
+# USAGE: ./pdfmerge.sh file1.pdf file2.pdf output_file.pdf
 #
 # Comes handy when you're applying for a job and can only reach in ONE pdf file.
 # I know it could be more fancy, like recursively merging all pdf's in one folder.
