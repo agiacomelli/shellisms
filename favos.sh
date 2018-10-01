98ffabffbbb8cd49f5e3e5c68669925b01d2e143
agiacomelli <36065626+agiacomelli@users.noreply.github.com>
2018-10-01 14:59:42 +0200

    Update favos.sh

--- favos.sh
+++ favos.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-# the list of my favourite linux apps and how i install them on a new arch system with just one cmd
-# cherrytree
-# owncloud-client
-# opera
-# geany
-# geary
-# .....
+# the list of my favourite linux apps
+# easy install on a new arch sys with just one cmd
 
-sudo pacman -S cherrytree owncloud-client opera filezilla chromium geany geary processing go nodejs npm atom scribus inkscape mplayer whois curl neofetch pluma vim tor
+CODING="processing ruby nodejs go geany atom npm "
+OFFICE="libreoffice cherrytree keepnote "
+NET="geary opera firefox telegram-desktop filezilla owncloud-client curl whois net-tools tor macchanger "
+MULTIMEDIA="scribus mplayer clementine inkscape "
+MISC="zsh neofetch pluma"
+
+sudo pacman -S $CODING $OFFICE $NET $MULTIMEDIA $MISC
