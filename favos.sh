ce5a493f05c83bde9990ce20fdf3d351c6b2cfbe
agiacomelli <36065626+agiacomelli@users.noreply.github.com>
2018-10-03 08:45:06 +0200

    Update favos.sh

--- favos.sh
+++ favos.sh
@@ -4,9 +4,9 @@
 # easy install on a new arch sys with just one cmd
 
 CODING="processing ruby nodejs go geany atom npm "
-OFFICE="libreoffice cherrytree keepnote "
-NET="geary opera firefox telegram-desktop filezilla owncloud-client curl whois net-tools tor macchanger "
-MULTIMEDIA="scribus mplayer clementine inkscape "
+OFFICE="libreoffice cherrytree keepnote vym "
+NET="geary opera firefox telegram-desktop filezilla owncloud-client curl whois net-tools tcpdump tor macchanger "
+MULTIMEDIA="scribus mplayer clementine inkscape xsane "
 MISC="zsh neofetch pluma"
 
 sudo pacman -S $CODING $OFFICE $NET $MULTIMEDIA $MISC
